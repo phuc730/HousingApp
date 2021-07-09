9aa67417bbb997ed684f8cbc3c76bfd250a72da0
phuc730 <66136174+phuc730@users.noreply.github.com>
2021-07-09 19:07:54 +0700

    design add property form

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -19,6 +19,9 @@ import { AlertifyService } from './services/alertify.service';
 import { AuthService } from './services/auth.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
@@ -48,7 +51,10 @@ const appRoutes: Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ButtonsModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   providers: [
     HousingService,
--- FrontEnd/src/app/property/add-property/add-property.component.ts
+++ FrontEnd/src/app/property/add-property/add-property.component.ts
@@ -1,6 +1,9 @@
+import { isNull } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { IProperty } from '../property-list/IProperty.interface';
 
 @Component({
   selector: 'app-add-property',
@@ -9,6 +12,19 @@ import { Router } from '@angular/router';
 })
 export class AddPropertyComponent implements OnInit {
   @ViewChild('Form') addPropertyForm! : NgForm ;
+  @ViewChild('formTabs', { static: false }) formTabs!: TabsetComponent;
+
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Deplex'];
+  purnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
+  locationToMoves: Array<string> = ['East', 'West', 'South', 'North'];
+  propertyView: IProperty = {
+    Id: null,
+    Name: "",
+    Price: null,
+    SellRent: null,
+    Type: "",
+    Image: "p1"
+  };
   constructor(private route : Router) { }
 
   ngOnInit() {
@@ -22,4 +38,7 @@ export class AddPropertyComponent implements OnInit {
     console.log(this.addPropertyForm);
   }
 
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
+  }
 }
